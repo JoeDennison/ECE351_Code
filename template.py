360cddd0024daf5e23a65e45f3a281685c5950c0
JoeDennison <denn2258@vandal.uidaho.edu>
2020-01-26 13:23:17 -0800

    Create template.py

---
+++ template.py
@@ -0,0 +1,22 @@
+################################################
+#                                              #
+# Joseph Dennison                              #
+# ECE 351-51                                   #
+# Lab 0                                        #
+# 1/21/20                                      #
+#                                              #
+#################################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sig
+import control as con
+import pandas as pd
+
+#Question 1
+#I didn't to anything productive last summer
+#This summer I plan for an internship either in the analog or digital field
+
+#Question 2
+#Personally, I want to inprove my python, Latex, and Github skills
+#Change the wording from familiar to knowledgeable in my resume
